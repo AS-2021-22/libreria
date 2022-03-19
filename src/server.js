af5a9f63a29807a68bddb0e0014cac4f715c7c1e
Carminati Giovanni <Carminati.17090>
2022-03-19 09:45:20 +0100

    done server side

--- src/server.js
+++ src/server.js
@@ -34,11 +34,49 @@ DB.connect((err) => {
     }
 })
 
-app.get('/',(_,res) => {
-    res.render(path.join(htmlFolder,'index.html'),{nome:'hello world'})
+
+app.route('/')
+
+    .get((_,res) => {
+        res.render(path.join(htmlFolder,'index.html'),{nome:'hello world'})
+    })
+
+    .post((req,res) => {
+        const {ISBN,autore,titolo} = req.body
+        const sql = /*sql*/`INSERT INTO libro (ISBN,autore,titolo) VALUES ("${ISBN}","${autore}","${titolo}")`
+        DB.query(sql,(err) =>{
+            if(err) res.status(500).json({'sql error':e.message})
+            else res.status(200)
+        })
+    })
+
+    .put((req,res)=>{
+        const {ISBN,autore,titolo} = req.body
+        const sql = /*sql*/`UPDATE libro SET titolo = "${titolo}", autore = "${autore}" WHERE ISBN = "${ISBN}"`
+        DB.query(sql,(err) =>{
+            if(err) res.status(500).json({'sql error':e.message})
+            else res.status(200)
+        })
+        res.status(200).json()
+    })
+
+app.delete('/:ISBN',(req,res) =>{
+    const {ISBN} = req.params
+    const sql = /*sql*/`DELETE FROM libri WHERE ISBN = "${ISBN}"`
+    DB.query(sql,(err)=>{
+        if(err) res.status(500).json({'sql error':e.message})
+        else res.status(200)
+    })
 })
 
 
+app.post('/get_libri',(req,res) => {
+    const sql = /*sql*/`SELECT * FROM libri ${req.filter || ''}`
+    DB.query(sql,(err,result)=>{
+        if(err) res.status(500).json({'sql error':e.message})
+        else res.status(200).json(result)
+    })
+})
 
 
 
